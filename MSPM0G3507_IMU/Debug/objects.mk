54f7e2f31a3083da3b1f46c16957b90e8e6bb10f
wara <joewww233@gmail.com>
2026-07-12 22:53:37 +0800

    Add initial CMSIS DSP empty project for MSPM0G3507
    - Created README.md to provide an overview of the example project, including peripherals, pin assignments, and low-power recommendations.
    - Implemented cmsis_dsp_empty.c as the main application file, initializing the system configuration and entering an infinite loop.
    - Added syscfg file for project configuration, enabling CMSIS library generation and configuring the SYSCTL module.
    - Introduced target configuration file (MSPM0G3507.ccxml) for XDS110 USB Debug Probe connection settings.
    - Included readme.txt in targetConfigs folder to explain the purpose and management of target-configuration files.

---
+++ MSPM0G3507_IMU/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -Wl,-Tdevice.lds.genlibs -l:arm_cortexM0l_math.a -l:driverlib.a -lgcc -lc -lm
+

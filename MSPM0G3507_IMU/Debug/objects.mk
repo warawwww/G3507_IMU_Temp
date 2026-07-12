bf5fdc8781ce9f9df8cc7762c6e43d2f973ab96b
wara <joewww233@gmail.com>
2026-07-13 01:05:41 +0800

    Remove target configuration files and associated documentation for MSPM0G3507_IMU

--- MSPM0G3507_IMU/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-Tdevice.lds.genlibs -l:arm_cortexM0l_math.a -l:driverlib.a -lgcc -lc -lm
-
--- MSPM0G3507_IMU/Debug/startup_mspm0g350x_gcc.d
+++
@@ -1,4 +0,0 @@
-# FIXED
-
-startup_mspm0g350x_gcc.o: \
- D:/diansai/ti/mspm0_sdk_2_10_00_04/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c
